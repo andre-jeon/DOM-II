e9929bc7ab84ff5103b814310e20d1d99f33ff93
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-07-07 21:03:18 -0400

    working on basic things

--- js/index.js
+++ js/index.js
@@ -1,6 +1,65 @@
 // Your code goes here
-const bigButton = document.querySelector('.btn');
-bigButton.addEventListener('mouseup', (event) => {
-    event.target.style.padding = '40px';
-});
 
+// 1. dblclick
+const logoHeading = document.querySelector('.logo-heading')
+
+logoHeading.addEventListener('dblclick', () => {
+    logoHeading.textContent = 'Funnier Bus🤣'
+  })
+
+// 2. wheel
+document.querySelector('.footer p').addEventListener('wheel', zoomFucntion);
+
+function zoomFucntion() {
+    this.style.fontSize = '35px'
+    this.textContent = 'YUH! YUH! YUH!'
+}
+
+// 3. mouseover
+document.querySelector(".intro h2").addEventListener("mouseover", mouseOver);
+
+function mouseOver() {
+    document.querySelector(".intro  h2").style.color = "red";
+  }
+
+// 4. mouseout
+document.querySelector(".intro  h2").addEventListener("mouseout", mouseOut);
+
+function mouseOut() {
+    document.querySelector(".intro  h2").style.color = "black";
+    }
+
+// 5. keydown
+document.addEventListener('keydown', logKey);
+let keyDownTesting = document.querySelector('.text-content')
+
+function logKey(e) {
+    keyDownTesting.textContent = `${e.code}`;
+}
+
+// 6. click
+document.querySelector(".nav-link").addEventListener("click", navFocusFunction);
+
+function navFocusFunction() {
+  document.querySelector(".nav-link").style.fontSize = "100px";
+}
+
+// 7. scroll
+// document.addEventListener("scroll", scrollFunction);
+
+// function scrollFunction() {
+//   alert("You scrolled!")
+// }
+
+// 8. drag
+document.addEventListener("dragstart", dragFunction)
+
+function dragFunction(){
+    alert("You dragged!")
+}
+
+// 9.
+
+
+
+// 10.
